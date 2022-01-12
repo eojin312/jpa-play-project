aea08149ece11050a15fa963153536dc4ac57923
이어진 <ejlee@hanatour.com>
2022-01-12 09:25:30 +0900

    일단 service 생성만 해둠 TODO : 1/12 저녁에 jpa 따라하면서 CRUD 해보기, spring in action 보고 따라하기

---
+++ src/main/java/captain/avengersproject/sample/service/SampleService.java
@@ -0,0 +1,9 @@
+package captain.avengersproject.sample.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SampleService {
+
+    String bars();
+}
---
+++ src/main/java/captain/avengersproject/sample/service/impl/SampleServiceImpl.java
@@ -0,0 +1,15 @@
+package captain.avengersproject.sample.service.impl;
+
+import captain.avengersproject.sample.service.SampleService;
+
+/**
+ * created on ej [2021.1.12]
+ * jpa 연습하는 연습실
+ */
+public class SampleServiceImpl implements SampleService {
+
+    @Override
+    public String bars() {
+        return "82Bars";
+    }
+}
